53fbf327444e3d31c295760c3b3d6477dc114c19
Ruslan Vaitukevich <rusikruslanchik@mail.ru>
2024-06-27 18:14:34 +0300

    lab06 done

--- labs/main.cpp
+++ labs/main.cpp
@@ -4,13 +4,34 @@
 #include "readAndParse.h"
 
 int main(int argc, char* argv[]) {
-    std::string path_str = argv[1];
-    std::filesystem::path path(path_str);
+    try {
+        std::string source_path = argv[1];
+        std::string destination_path = argv[2];
+        std::filesystem::path source(source_path);
+        std::filesystem::path destination(destination_path);
 
-    std::cout << '\n';
+        std::cout << '\n';
 
-    CheckArgumentsAmount(argc);
-    CheckInputPathLab5(path);
-    outputForLab5(path);
-    return 0;
+        CheckArgumentsAmountLab6(argc);
+        CheckDirectoryPath(source);
+        CheckDirectoryPath(destination);
+        std::set<std::string> content = GetFilesContentFromDirectory(destination);
+        for (const auto& entry: std::filesystem::directory_iterator(source)) {
+            if (is_regular_file(entry.path())) {
+                std::string fileContent = ReadFileContent(entry.path());
+                auto check = content.find(fileContent);
+                if (check == content.end()) {
+                    content.insert(fileContent);
+                    std::filesystem::copy_file(entry.path(), destination / entry.path().filename());
+                    std::string outMessage = "File by path " + entry.path().string() +
+                                             " has been copied to directory by path " +
+                                             destination.string();
+                    std::cout << outMessage << '\n';
+                }
+            }
+        }
+        return 0;
+    } catch (const std::exception& error) {
+        std::cerr << error.what() << '\n';
+    }
 }
